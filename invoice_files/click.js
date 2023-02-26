76e2034f1a3f90a62c631f2d43c7ba48bf1de078
hamudev <124338737+hamudev@users.noreply.github.com>
2023-02-26 19:40:24 +0800

    Add files via upload

---
+++ invoice_files/click.js
@@ -0,0 +1,6 @@
+document.getElementById("open_pp").addEventListener("click", () => {
+  $(".x_page").show();
+});
+document.getElementById("close_pp").addEventListener("click", () => {
+  $(".x_page").hide();
+});
